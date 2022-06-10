fd9a97d924d985a908208782f88285e3ebd50a42
Damilola A.J <iamroot@kali.kali>
2022-06-10 22:38:55 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const container = document.querySelector(".container"),
+      pwShowHide = document.querySelectorAll(".showHidePw"),
+      pwFields = document.querySelectorAll(".password"),
+      signUp = document.querySelector(".signup-link"),
+      login = document.querySelector(".login-link");
+
+
+    pwShowHide.forEach(eyeIcon =>{
+        eyeIcon.addEventListener("click", ()=>{
+            pwFields.forEach(pwField =>{
+                if(pwField.type ==="password"){
+                    pwField.type = "text";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye-slash", "uil-eye");
+                    })
+                }else{
+                    pwField.type = "password";
+
+                    pwShowHide.forEach(icon =>{
+                        icon.classList.replace("uil-eye", "uil-eye-slash");
+                    })
+                }
+            }) 
+        })
+    })
+
+
+
+    
+    signUp.addEventListener("click", ( )=>{
+        container.classList.add("active");
+    });
+    login.addEventListener("click", ( )=>{
+        container.classList.remove("active");
+    });
+
+    // function disableSubmit() {
+    //     document.getElementById("submit").disabled = true;
+    //    }
+      
+    //     function activateButton(element) {
+      
+    //         if(element.checked) {
+    //           document.getElementById("submit").disabled = false;
+    //          }
+    //          else  {
+    //           document.getElementById("submit").disabled = true;
+    //         }
+      
+    //     }
+
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+  
+        function OTPInput() {
+      const inputs = document.querySelectorAll('#otp > *[id]');
+      for (let i = 0; i < inputs.length; i++) { inputs[i].addEventListener('keydown', function(event) { if (event.key==="Backspace" ) { inputs[i].value='' ; if (i !==0) inputs[i - 1].focus(); } else { if (i===inputs.length - 1 && inputs[i].value !=='' ) { return true; } else if (event.keyCode> 47 && event.keyCode < 58) { inputs[i].value=event.key; if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault(); } else if (event.keyCode> 64 && event.keyCode < 91) { inputs[i].value=String.fromCharCode(event.keyCode); if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault(); } } }); } } OTPInput();
+      
+          
+      });
